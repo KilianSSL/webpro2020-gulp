5de8169ebc1ae51c22b5b98df3061233049cc63d
Kilian Schefer <kilian.schefer@liip.ch>
2020-04-04 11:54:55 +0200

    Extend gulp setup

---
+++ src/js/helper.js
@@ -0,0 +1,19 @@
+export function test() {
+  console.log("test");
+}
+
+export function test2() {
+  console.log();
+}
+
+export function test3() {
+  console.log("test");
+}
+
+export function test4() {
+  console.log("test");
+}
+
+export function test5() {
+  console.log("test");
+}
